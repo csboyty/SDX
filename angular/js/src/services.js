78d8bccfa7ee337bc49421ad165e719179f753d7
csboyty <csboyty@gmail.com>
2016-05-14 14:04:27 +0800

    ty

--- angular/js/src/services.js
+++ angular/js/src/services.js
@@ -27,14 +27,9 @@ services.constant("Config",{
         systemError:"系统发生错误，请稍后重试！"
     },
     ajaxUrls:{
-<<<<<<< HEAD
     	//baseUrl:"",
         //baseUrl:"http://192.168.2.6:8080/sdx/",
-       	baseUrl:"http://42.121.145.120:7000/sdx/",
-=======
-        baseUrl:"http://42.121.145.120:7000/sdx/",
-        //baseUrl:"",
->>>>>>> origin/master
+       	baseUrl:"http://42.121.145.120:7000/sdx/",   
         submitSelect:"user/insert",
         //clothesLoad:"data/json/items.php"
         clothesLoad:"style/list"
